23c3f3a156c3144eb422c25eec0679fe8514c85f
Sebastian Krzyszkowiak <dos@dosowisko.net>
2011-06-20 21:40:13 +0200

    update default config

--- conf.example/config.php
+++ conf.example/config.php
@@ -1,5 +1,10 @@
 <?
-$_CONFIG['sitename']='Sówka';
-$_CONFIG['siteurl']='http://localhost/sowka/';
-$_CONFIG['theme']='default';
-$_CONFIG['comics_path']='strips/';
+$_CONFIG['sitename']    = 'Sówka';
+$_CONFIG['siteurl']     = 'http://localhost/';
+$_CONFIG['theme']       = 'default';
+$_CONFIG['comics_path'] = 'strips/';
+
+$_CONFIG['mysql_host']  = 'localhost';
+$_CONFIG['mysql_user']  = 'user';
+$_CONFIG['mysql_pass']  = 'password';
+$_CONFIG['mysql_db']    = 'sowka';
