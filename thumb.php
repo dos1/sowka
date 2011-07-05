a53bc0eef060806cdfb8f29c03cc7778b76a1cb4
Sebastian Krzyszkowiak <dos@dosowisko.net>
2011-07-05 18:19:18 +0200

    fix transparent pngs, make thubnails interlaced

--- thumb.php
+++ thumb.php
@@ -6,14 +6,28 @@ if (!file_exists($_GET['file'])) {
   die();
 }
 
+if ($_GET['width']>2000) {
+  echo "Too big.";
+  die();
+}
+
 $size = getimagesize($_GET['file']);
 //header('Content-type: '.$size['mime']);
 
 $height = $_GET['width'] / ($size[0]/$size[1]);
 
+if ($size[2]==1) {
+  $ext = '.gif';
+}
+elseif ($size[2]==2) {
+  $ext = '.jpg';
+}
+elseif ($size[2]==3) {
+  $ext = '.png';
+}
 
-if (file_exists("thumbnails/".md5($_GET['file'].".".$_GET['width']))) {
-header('Location: '.$_CONFIG['siteurl'].'thumbnails/'.md5($_GET['file'].".".$_GET['width']));
+if (file_exists("thumbnails/".md5($_GET['file'].".".$_GET['width']).$ext)) {
+header('Location: '.$_CONFIG['siteurl'].'thumbnails/'.md5($_GET['file'].".".$_GET['width']).$ext);
 die();
 }
 
@@ -31,16 +45,19 @@ else {
 }
 
 $obraznowy=imagecreatetruecolor($_GET['width'],$height);
+imageinterlace($obraznowy, true);
+imagealphablending($obraznowy, false);
+imagesavealpha($obraznowy, true);
 imagecopyresized($obraznowy,$obraz,0,0,0,0,$_GET['width'],$height,$size[0],$size[1]);
 
 if ($size[2]==1) {
-  imagegif($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']));
+  imagegif($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']).$ext);
 }
 elseif ($size[2]==2) {
-  imagejpeg($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']),80);
+  imagejpeg($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']).$ext,80);
 }
 elseif ($size[2]==3) {
-  imagepng($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']));
+  imagepng($obraznowy,"thumbnails/".md5($_GET['file'].".".$_GET['width']).$ext);
 }
 
-header('Location: '.$_CONFIG['siteurl'].'thumbnails/'.md5($_GET['file'].".".$_GET['width']));
+header('Location: '.$_CONFIG['siteurl'].'thumbnails/'.md5($_GET['file'].".".$_GET['width']).$ext);
