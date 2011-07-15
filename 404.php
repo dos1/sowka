4d79f2fbcc1ca057a1ee44355fe1065e979b3596
Sebastian Krzyszkowiak <dos@dosowisko.net>
2011-07-16 00:15:53 +0200

    woring in 404 page

--- 404.php
+++ 404.php
@@ -3,6 +3,6 @@ include_once('includes/functions.php');
 
 $page_title='404';
 $error['title']='404 Not Found';
-$error['content']='Błąd 404 - Sówka nie istnieje.';
+$error['content']='<h3>Błąd 404 - Sówka nie istnieje</h3><p>...to znaczy, nie znalazł strony. Choć szukał.</p>';
 
 include_once('themes/'.$_CONFIG['theme'].'/error.php');
