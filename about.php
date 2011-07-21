82f9ac594c95594674202188880f428cb57182bb
Sebastian Krzyszkowiak <dos@dosowisko.net>
2011-07-21 13:20:18 +0200

    more work on about page

--- about.php
+++ about.php
@@ -5,26 +5,27 @@ $page_title='O Sówce';
 $about['title']='O Sówce';
 $about['content']='
 <img src="/images/about/6.png" style="float: left" width="125"/>
-<img src="/images/about/7.png" style="float: right" width="140"/> 
-<!--<img src="/images/logo.png" style="float: left" width="60"/>-->
+<img src="/images/about/7.png" style="float: right" width="140"/>
 <p>O Sówce jako bohaterze możesz sporo się dowiedzieć przeglądając komiks - tu natomiast będzie trochę o samym projekcie, który po Sówce odziedziczył m.in. swoją nazwę.</p>
-<p>Projekt Sówka zrodził się w formie prostych szkiców rysowanych gdzie popadnie (głównie w książce od angielskiego...). Zeszyty, kartki, książki, <a href="/images/about/tablica.jpg">tablice</a> - wszędzie pojawiały się różnorodne ptaszki, reprezentujące wiele gatunków i środowisk. Wszystko to za sprawą Agaty Kurczewskiej - autorki Sówki.</p>
+<p>Projekt Sówka zrodził się w formie prostych szkiców rysowanych gdzie popadnie (głównie w książce od angielskiego...). Zeszyty, kartki, książki, <a href="/images/about/tablica.jpg">tablice</a> - wszędzie pojawiały się różnorodne ptaszki, reprezentujące <a href="/images/about/5.png">wiele gatunków</a> i <a href="/images/about/2.png">środowisk</a>. Czasem nawet te rysunki układały się w <a href="/images/about/1.png">krótkie</a> <a href="/images/about/4.png">paski</a>. Wszystko to za sprawą Agaty Kurczewskiej - autorki Sówki.</p>
 <p>Sówek zaczęło pojawiać się więcej, znacznie więcej od innych papug, wróbli czy gołębi. Po pewnym czasie zrodziła się ta jedna, jedyna sówka - Sówka. Na przestrzeni wielu rysunków i szkiców ukształtował się jego charakter, światopogląd, wygląd, upierzenie, a nawet płeć.</p>
 <p><img src="/images/about/9.png" width="400"/></p>
 <p>Po jakimś czasie Sówce przestały wystarczać szkice. Nie zadowolił się nawet <a href="/images/about/plastelina.jpg">modelem z plasteliny</a>. Chciał więcej - a tak się akurat złożyło, że była ku temu okazja. Otóż przyjaciółki Sówki - sówki międzymordki - wzięły gościnny udział w... prezentacji maturalnej. Sebastian Krzyszkowiak, ówczesny maturzysta, przedstawiał problem zapożyczeń językowych używanych w słownictwie informatycznym. Uczynił to właśnie przy pomocy sówek, które wyszły spod ręki Agaty. Sówki z różnych krajów odkryły, że łatwiej jest się im porozumiewać internacjonalizmami.</p>
 <p style="text-align: center; clear: both">
-<img src="http://dosowisko.net/miedzymordzie_z_kolorami.png" width="350"/>
-<img src="http://dosowisko.net/miedzymordzie_z_kolorami2.png" width="350"/>
+<img src="/images/about/miedzymordki.png" width="350"/>
+<img src="/images/about/miedzymordki2.png" width="350"/>
 </p>
 
-<img src="/images/about/8.png" style="float: right" width="80"/> 
+<img src="/images/about/8.png" style="float: right; margin-top: 20px;" width="80"/> 
 <p>Na szczęście nie był to koniec artystycznej współpracy Agaty z Sebastianem. Pomysł na stworzenie komiksu internetowego błądził po głowie "sowiej mamy" już wcześniej, jednak właśnie współpraca z kimś, kto programowaniem zajmuje się na codzień, pozwoliła zrealizować ten cel. Udało się - 27 lipca 2011 roku strona Sówki miała swoją premierę, a wraz z nią opublikowany został również pierwszy pasek komiksu.</p>
 <p>Kiedy strona Sówki dopiero nabierała swoich kształtów, swą pomoc zaoferował Adrian Grzemski ze swoim <a href="http://adriangrzemski.blogspot.com/">blogiem</a>, stając się partnerem Sówki. Kto wie, może Sówka zainspiruje go przy któreś z jego przyszłych publikacji? :)</p>
 <p>A sam Sówka? <a href="/blog/1/">Cieszy się</a>, że ma swoją stronę w tym olbrzymim, ludzkim Internecie. A to i tak jeszcze nie jest jego ostatnie słowo...</p>
-<p style="text-align: center; clear: both">
-<img src="/images/about/3.png" height="140"/>
+<p class="bottom">
+<img src="/images/about/soweczka.png" height="140"/>
 <img src="/images/about/10.png" height="140"/>
 <img src="/images/about/14.png" height="140"/>
+<img src="/images/about/3.png" height="140"/>
+<img src="/images/about/rambo.png" height="140"/>
 </p>
 ';
 
